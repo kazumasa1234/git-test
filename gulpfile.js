517ecd57ce95783808a604bcc1e31ad1afed3435
k.fukui <kazumasa.f@docomonet.jp>
2021-07-17 17:07:58 +0900

    addしました

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+// var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass', function() {
+    return gulp
+        .src('./sass/**/*.scss')
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(gulp.dest('./css'));
+});
