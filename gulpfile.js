6a5d16ebebe82325cc6fa7dbe09f67d0e412ae7d
k.fukui <kazumasa.f@docomonet.jp>
2021-07-17 18:52:03 +0900

    ローカルサーバー立ち上げ　ブラウザ自動リロード

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,20 @@ var webserver = require('gulp-webserver');
 gulp.task('default', function() {
     gulp.src('app')
         .pipe(webserver({
-            port: 9000
+            port: 9000,
+            livereload: true,
+
         }));
 });
+
+
+//サンプルコード
+// gulp.task("default", function() {
+//     return gulp.src('public')
+//         .pipe(webserver({
+// このした自動リロード　コマンドsでセーブしたらブラウザ更新される
+//             livereload: true,
+//この下は起動時に自動で起動するかどうか　多分ブラウザ？
+//             open: true
+//         }));
+// });
