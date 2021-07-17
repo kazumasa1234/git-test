ad1c7791e7eda21902c604c483ec1620879c73d9
k.fukui <kazumasa.f@docomonet.jp>
2021-07-17 18:19:56 +0900

    サーバー立ち上げ

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,21 @@ var gulp = require('gulp');
 // var sass = require('gulp-sass');
 var sass = require('gulp-sass')(require('sass'));
 
+var webserver = require('gulp-webserver');
+
 gulp.task('sass', function() {
     return gulp
         .src('./sass/**/*.scss')
         .pipe(sass({outputStyle: 'expanded'}))
         .pipe(gulp.dest('./css'));
+});
+
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+
+gulp.task('default', function() {
+    gulp.src('app')
+        .pipe(webserver({
+            port: 9000
+        }));
 });
